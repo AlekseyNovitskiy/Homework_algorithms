b4d96285e75987d459d110c86d7072d78da694de
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2022-11-26 21:39:37 +0300

    ДЗ Основы алгоритмов, ч. 2 готово

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,69 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Random random = new Random();
+        int[] arr = new int[100_000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(200_000);
+        }
+        int[] arr2= Arrays.copyOf(arr, arr.length);;
+
+        int[] arr3 = Arrays.copyOf(arr, arr.length);
+
+        long start = System.currentTimeMillis();
+        sortBubble(arr);
+        System.out.println(System.currentTimeMillis() - start);
+        long start2 = System.currentTimeMillis();
+        sortSelection(arr2);
+        System.out.println(System.currentTimeMillis() - start2);
+        long start3 = System.currentTimeMillis();
+        sortInsertion(arr3);
+        System.out.println(System.currentTimeMillis() - start3);
+    }
+    private static void swapElements(int[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+    public static void sortBubble(int[] arr) {     // 26868
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapElements(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void sortSelection(int[] arr) {   // 7664
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minElementIndex]) {
+                    minElementIndex = j;
+                }
+            }
+            swapElements(arr, i, minElementIndex);
+        }
+    }
+    public static void sortInsertion(int[] arr) {  //  1984
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
     }
 
 }
+
+
